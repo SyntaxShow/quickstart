e90eee28305271b4c4b03108e450866ce58b0b9a
SyntaxShow <alihorabadi78@gmail.com>
2024-08-09 00:18:28 +0330

    models add to db

--- root/admin.py
+++ root/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Services
+from .models import *
+admin.site.register(Special_services)
 admin.site.register(Services)
-
-
+admin.site.register(Attributes)
+admin.site.register(Pricings)
+admin.site.register(Questions)
+admin.site.register(Skills)
+admin.site.register(Trainer)
 # Register your models here.
--- root/models.py
+++ root/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Services(models.Model):
+class Special_services(models.Model):
     name = models.CharField(max_length=120)
-    content = models.TextField(default="Test")
+    content = models.TextField(default="description special services")
     status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,4 +14,92 @@ class Services(models.Model):
     class Meta:
         ordering = ("created_at",)
 
+
+
+class Services(models.Model):
+    name = models.CharField(max_length=120)
+    content = models.TextField(default="description services")
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("created_at",)
+
+
+
+class Attributes(models.Model):
+    name = models.TextField(default="content attribute")
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ("created_at",)
+
+
+
+class Pricings(models.Model):
+    name = models.CharField(max_length=120)
+    content = models.TextField(default="description pricing")
+    fee = models.FloatField(default=3)
+    attribute = models.ManyToManyField(Attributes)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("created_at",)
+
+
+
+class Questions(models.Model):
+    question = models.TextField(default="title question?")
+    answer = models.TextField(default="answer question")
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ("created_at",)
+
+
+
+class Skills(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ("created_at",)
+
+class Trainer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="trainer",default="default.jpg")
+    content = models.TextField(default="description trainer")
+    skills = models.ManyToManyField(Skills)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ("created_at",)
 # Create your models here.
